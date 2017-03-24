8b5eded6ac727c66ec7f172822ba54feeffab1d0
Jerome Bakker <jeabakker@coldtrick.com>
2017-03-24 15:32:25 +0100

    chore: updated Dutch translation

--- languages/nl.php
+++ languages/nl.php
@@ -1,7 +1,6 @@
 <?php
-$language = array (
+return array (
   'entity_view_counter:entity:menu:views' => '%d weergaven',
   'entity_view_counter:settings:description' => 'Om de weergaven van een entity te volgen moet je deze hieronder selecteren. Als je een nieuwe plugin activeert moet je hier misschien terug komen, omdat standaard nieuwe entity types niet worden gevolgd.',
   'entity_view_counter:settings:entity_type' => 'Entity type om te volgen',
 );
-add_translation("nl", $language);
